9d155690dc5838f595fb40b72a78361b05157fcf
Archit <archit.jaincs123@gmail.com>
2020-09-18 03:48:45 +0530

    Added character count functionality

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
-
+class TweetViewController: UIViewController, UITextViewDelegate {
+    
     @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var charCountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTextView.becomeFirstResponder()
+        tweetTextView.delegate = self
     }
     
     @IBAction func onCancel(_ sender: Any) {
@@ -30,5 +32,14 @@ class TweetViewController: UIViewController {
             })
         }
     }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let characterLimit = 280
+        let newText = NSString(string: textView.text!).replacingCharacters(in: range, with: text)
+        if (newText.count <= characterLimit){
+            charCountLabel.text = "\(newText.count)/280"
+        }
+        return newText.count <= characterLimit
+    }
 }
- 
+
